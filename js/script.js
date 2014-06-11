d484520ecc0f924998cf83d498152f2fe9bdbf77
iahy <i98102016@gmail.com>
2014-06-11 17:11:02 +0800

    kg3k

--- js/script.js
+++ js/script.js
@@ -120,7 +120,14 @@ window.fbAsyncInit = function () {
 	js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=320206518133001&version=v2.0";
 	fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
-    		
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=320206518133001&version=v2.0";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+	
 function PostImageToFacebook(authToken) {
 	$('.info').append('<img src="img/loading.gif"/>')
     var canvas = document.getElementById("canvas");
